fb418dbd0e919f2243679bc30cd9f62e775d2b22
bacbarbi28 <bacbarbi@gmail.com>
2025-04-12 16:16:43 +0200

    🐛 bugfix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,10 @@ import { MatButtonModule } from '@angular/material/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatToolbarModule, RouterModule,MatCardModule, MatButtonModule],
+  imports: [MatToolbarModule, RouterModule, MatCardModule, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'TotallyNotMvm';
+  title = 'forma1';
 }
